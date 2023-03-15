73187c36db594735cdcc2bd25f321992dc5bedd1
Yegor Bugayenko <yegor256@gmail.com>
2023-03-15 10:54:51 +0800

    #xi: self instead of static

--- src/universe.rs
+++ src/universe.rs
@@ -165,71 +165,70 @@ impl Universe {
         if a == "Φ" {
             return Ok("ν0".to_string());
         };
-        let v1 = Self::fnd(uni, v, a, 0)?;
+        let v1 = uni.fnd(v, a, 0)?;
         Ok(format!("ν{v1}"))
     }
 
     /// Find.
-    fn fnd(uni: &mut Universe, v: u32, a: &str, psi: u32) -> Result<u32> {
-        Self::check_recursion(uni)?;
-        let v1 = Self::dd(uni, v, psi)?;
+    fn fnd(&mut self, v: u32, a: &str, psi: u32) -> Result<u32> {
+        self.check_recursion()?;
+        let v1 = self.dd(v, psi)?;
         trace!("#fnd(ν{v}, {a}, {psi}): dd(ν{v}) returned ν{v1}");
-        let to = Self::pf(uni, v1, a, psi)?;
+        let to = self.pf(v1, a, psi)?;
         trace!("#fnd(ν{v}, {a}, {psi}): pf(ν{v}, {a}) returned ν{to}");
-        uni.depth -= 1;
+        self.depth -= 1;
         Ok(to)
     }
 
     /// Path find.
-    fn pf(uni: &mut Universe, v: u32, a: &str, psi: u32) -> Result<u32> {
-        Self::check_recursion(uni)?;
+    fn pf(&mut self, v: u32, a: &str, psi: u32) -> Result<u32> {
+        self.check_recursion()?;
         trace!("#pf(ν{v}, {a}, {psi}): entering...");
-        let r = if let Some(to) = uni.g.kid(v, a) {
+        let r = if let Some(to) = self.g.kid(v, a) {
             Ok(to)
-        } else if let Some(to) = uni.g.kid(v, "ε") {
-            Self::fnd(uni, to, a, psi)
-        } else if let Some(to) = uni.g.kid(v, "ξ") {
-            Self::fnd(uni, to, a, psi)
-        } else if let Some(lv) = uni.g.kid(v, "λ") {
-            let lambda = uni.g.data(lv)?.to_utf8()?;
+        } else if let Some(to) = self.g.kid(v, "ε") {
+            self.fnd(to, a, psi)
+        } else if let Some(to) = self.g.kid(v, "ξ") {
+            self.fnd(to, a, psi)
+        } else if let Some(lv) = self.g.kid(v, "λ") {
+            let lambda = self.g.data(lv)?.to_utf8()?;
             trace!("#re: calling ν{v}.λ⇓{lambda}(ξ=ν?)...");
-            let to = uni
+            let to = self
                 .atoms
                 .get(lambda.as_str())
                 .context(anyhow!(
                     "Can't find function {lambda} among {} others",
-                    uni.atoms.len()
+                    self.atoms.len()
                 ))
-                .unwrap()(uni, v)?;
+                .unwrap()(self, v)?;
             trace!("#re: ν{v}.λ⇓{lambda}(ξ=ν?) returned ν{to}");
-            Self::fnd(uni, to, a, psi)
-        } else if let Some(to) = uni.g.kid(v, "φ") {
-            Self::fnd(uni, to, a, psi)
-        } else if let Some(to) = uni.g.kid(v, "γ") {
-            let t = Self::fnd(uni, to, a, psi)?;
-            uni.g.bind(v, t, a)?;
+            self.fnd(to, a, psi)
+        } else if let Some(to) = self.g.kid(v, "φ") {
+            self.fnd(to, a, psi)
+        } else if let Some(to) = self.g.kid(v, "γ") {
+            let t = Self::fnd(self, to, a, psi)?;
+            self.g.bind(v, t, a)?;
             Ok(t)
         } else {
             Err(anyhow!(
                 "There is no way to get .{a} from {}",
-                uni.g.v_print(v)
+                self.g.v_print(v)
             ))
         };
-        uni.depth -= 1;
+        self.depth -= 1;
         r
     }
 
     /// Dynamic dispatch.
-    fn dd(uni: &mut Universe, v: u32, psi: u32) -> Result<u32> {
-        Self::check_recursion(uni)?;
+    fn dd(&mut self, v: u32, psi: u32) -> Result<u32> {
+        self.check_recursion()?;
         trace!("#dd(ν{v}, {psi}): entering...");
-        uni.depth += 1;
-        let r = if let Some(to) = uni.g.kid(v, "ε") {
-            Self::dd(uni, to, psi)
-        } else if uni.g.kid(v, "ξ").is_some() {
-            Self::dd(uni, psi, psi)
-        } else if let Some(to) = uni.g.kid(v, "β") {
-            let a = uni
+        let r = if let Some(to) = self.g.kid(v, "ε") {
+            self.dd(to, psi)
+        } else if self.g.kid(v, "ξ").is_some() {
+            self.dd(psi, psi)
+        } else if let Some(to) = self.g.kid(v, "β") {
+            let a = self
                 .g
                 .kids(v)?
                 .iter()
@@ -237,90 +236,92 @@ impl Universe {
                 .unwrap()
                 .clone()
                 .0;
-            let nv = Self::fnd(uni, to, a.as_str(), psi)?;
-            Self::dd(uni, nv, psi)
-        } else if let Some(to) = uni.g.kid(v, "π") {
-            let psi2 = if let Some(p) = uni.g.kid(v, "ψ") {
+            let nv = self.fnd(to, a.as_str(), psi)?;
+            self.dd(nv, psi)
+        } else if let Some(to) = self.g.kid(v, "π") {
+            let psi2 = if let Some(p) = self.g.kid(v, "ψ") {
                 p
             } else {
                 v
             };
-            let nv = Self::dd(uni, to, psi2)?;
-            Self::apply(uni, nv, v)
+            let nv = self.dd(to, psi2)?;
+            self.apply(nv, v)
         } else {
             Ok(v)
         };
-        uni.depth -= 1;
+        self.depth -= 1;
         r
     }
 
     /// Apply `v1` to `v2` and return a new vertex.
-    fn apply(uni: &mut Universe, v1: u32, v2: u32) -> Result<u32> {
+    fn apply(&mut self, v1: u32, v2: u32) -> Result<u32> {
         trace!("#apply(ν{v1}, ν{v2}): entering...");
-        uni.depth += 1;
-        let nv = uni.add();
-        Self::pull(uni, nv, v1)?;
-        Self::push(uni, nv, v2)?;
+        self.depth += 1;
+        let nv = self.g.next_id();
+        self.g.add(nv)?;
+        self.pull(nv, v1)?;
+        self.push(nv, v2)?;
         trace!("#apply(ν{v1}, ν{v2}): copy ν{v1}+ν{v2} created as ν{nv}");
-        uni.depth -= 1;
+        self.depth -= 1;
         Ok(nv)
     }
 
     /// Pull into `v1` from `v2`.
-    fn pull(uni: &mut Universe, v1: u32, v2: u32) -> Result<()> {
-        for (a, k) in uni.g.kids(v2)?.into_iter() {
+    fn pull(&mut self, v1: u32, v2: u32) -> Result<()> {
+        for (a, k) in self.g.kids(v2)?.into_iter() {
             if a == "σ" || a == "β" || a == "π" {
                 continue;
             }
-            Self::up(uni, v1, k, a)?;
+            self.up(v1, k, a)?;
         }
         Ok(())
     }
 
     /// Link.
-    fn up(uni: &mut Universe, v1: u32, v2: u32, a: String) -> Result<()> {
-        if a == "λ" || a == "Δ" || a == "ρ" || Self::nil(uni, v2)? {
-            uni.g.bind(v1, v2, a.as_str())?;
+    fn up(&mut self, v1: u32, v2: u32, a: String) -> Result<()> {
+        if a == "λ" || a == "Δ" || a == "ρ" || self.nil(v2)? {
+            self.g.bind(v1, v2, a.as_str())?;
         } else {
-            let nv = uni.add();
-            uni.g.bind(v1, nv, a.as_str())?;
-            uni.g.bind(nv, v1, "ρ")?;
-            uni.g.bind(nv, v1, "ψ")?;
-            uni.g.bind(nv, v2, "π")?;
+            let nv = self.g.next_id();
+            self.g.add(nv)?;
+            self.g.bind(v1, nv, a.as_str())?;
+            self.g.bind(nv, v1, "ρ")?;
+            self.g.bind(nv, v1, "ψ")?;
+            self.g.bind(nv, v2, "π")?;
         };
         Ok(())
     }
 
     /// Push from `v2` to `v1`.
-    fn push(uni: &mut Universe, v1: u32, v2: u32) -> Result<()> {
-        for (a, k) in uni.g.kids(v2)?.into_iter() {
+    fn push(&mut self, v1: u32, v2: u32) -> Result<()> {
+        for (a, k) in self.g.kids(v2)?.into_iter() {
             if a == "π" {
                 continue;
             }
-            Self::down(uni, v1, k, a)?;
+            self.down(v1, k, a)?;
         }
         Ok(())
     }
 
     /// Link down.
-    fn down(uni: &mut Universe, v1: u32, v2: u32, a: String) -> Result<()> {
-        let a1 = Self::tie(uni, v1, a)?;
-        uni.g.bind(v1, v2, a1.as_str())?;
+    fn down(&mut self, v1: u32, v2: u32, a: String) -> Result<()> {
+        let a1 = self.tie(v1, a)?;
+        self.g.bind(v1, v2, a1.as_str())?;
         Ok(())
     }
 
     /// Tie an existing name with a new name.
-    fn tie(uni: &mut Universe, v: u32, a: String) -> Result<String> {
+    fn tie(&mut self, v: u32, a: String) -> Result<String> {
         if a == "ρ" || a == "ψ" || a == "σ" {
             trace!("#tie(ν{v}, {a}): it's a direct tie");
             return Ok(a);
         }
-        if a == "Δ" && uni.g.kid(v, a.as_str()).is_none() {
+        if a == "Δ" && self.g.kid(v, a.as_str()).is_none() {
             trace!("#tie(ν{v}, {a}): it's a new data");
             return Ok(a);
         }
-        if let Some(v1) = uni.g.kid(v, a.as_str()) {
-            if Self::nil(uni, v1)? {
+        if let Some(v1) = self.g.kid(v, a.as_str()) {
+            if self.nil(v1)? {
                 trace!("#tie(ν{v}, {a}): it's a nil");
                 return Ok(a);
             }
@@ -328,7 +329,7 @@ impl Universe {
         if a.starts_with('α') {
             let tail: String = a.chars().skip(1).collect::<Vec<_>>().into_iter().collect();
             let i = usize::from_str(tail.as_str())?;
-            let a1 = uni
+            let a1 = self
                 .g
                 .kids(v)?
                 .into_iter()
@@ -336,21 +337,21 @@ impl Universe {
                 .nth(i)
                 .unwrap();
             trace!("#tie(ν{v}, {a}): the {i}th attribute is {}", a1.0);
-            return Self::tie(uni, v, a1.0);
+            return self.tie(v, a1.0);
         }
         return Err(anyhow!("Can't tie to ν{v}.{a}"));
     }
 
     /// The vertex is a dead-end, a nil.
-    fn nil(uni: &mut Universe, v: u32) -> Result<bool> {
-        let kids = uni.g.kids(v)?;
+    fn nil(&mut self, v: u32) -> Result<bool> {
+        let kids = self.g.kids(v)?;
         return Ok(kids.len() == 1 && kids.iter().all(|(a, _)| a == "ρ"));
     }
 
-    fn check_recursion(uni: &mut Universe) -> Result<()> {
-        uni.depth += 1;
-        if uni.depth > 20 {
-            return Err(anyhow!("The recursion is too deep ({} levels)", uni.depth));
+    fn check_recursion(&mut self) -> Result<()> {
+        self.depth += 1;
+        if self.depth > 20 {
+            return Err(anyhow!("The recursion is too deep ({} levels)", self.depth));
         }
         Ok(())
     }
