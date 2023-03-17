f7d3af215370c7a640857f2e15d6d7dabd8fedbe
Yegor Bugayenko <yegor256@gmail.com>
2023-03-17 20:06:24 +0800

    print metas

--- src/bin/reo.rs
+++ src/bin/reo.rs
@@ -28,7 +28,7 @@ use clap::ErrorKind::EmptyValue;
 use clap::{crate_version, value_parser, AppSettings, Arg, ArgAction, Command};
 use colored::Colorize;
 use itertools::Itertools;
-use log::{debug, info, LevelFilter};
+use log::{debug, info, LevelFilter, warn};
 use reo::org::eolang::register;
 use reo::Universe;
 use simple_logger::SimpleLogger;
@@ -268,7 +268,7 @@ pub fn main() -> Result<()> {
             info!("Deployed {ints} instructions from {}", src.display());
             let size = g.save(bin)?;
             info!("The SODG saved to '{}' ({size} bytes)", bin.display());
-            print_metas(&mut g);
+            print_metas(&mut g)?;
         }
         Some(("empty", subs)) => {
             let bin = subs
@@ -276,7 +276,8 @@ pub fn main() -> Result<()> {
                 .context("Path of .reo file is required")
                 .unwrap();
             debug!("target: {}", bin.display());
-            let g = Sodg::empty();
+            let mut g = Sodg::empty();
+            g.add(0)?;
             let size = g.save(bin)?;
             info!("The SODG saved to '{}' ({size} bytes)", bin.display());
         }
@@ -297,12 +298,12 @@ pub fn main() -> Result<()> {
             if !source.exists() {
                 return Err(anyhow!("The file '{}' not found", source.display()));
             }
-            info!("Merging into '{}'", target.display());
+            info!("Merging into '{}':", target.display());
             let mut g1 = Sodg::load(target)?;
-            print_metas(&mut g1);
-            info!("Merging from '{}'", source.display());
+            print_metas(&mut g1)?;
+            info!("Merging from '{}':", source.display());
             let mut g2 = Sodg::load(source)?;
-            print_metas(&mut g2);
+            print_metas(&mut g2)?;
             g1.merge(&g2.slice_some("ν0", |_, _, a| !a.starts_with('+'))?, 0, 0)?;
             let size = g1.save(target)?;
             info!("The SODG saved to '{}' ({size} bytes)", target.display());
@@ -378,6 +379,8 @@ pub fn main() -> Result<()> {
             println!("Size: {} bytes", fs::metadata(bin)?.len());
             let mut g = Sodg::load(bin.as_path())?;
             println!("Total vertices: {}", g.len());
+            println!("Metas:");
+            print_metas(&mut g)?;
             let root = subs.get_one::<String>("root").unwrap().parse().unwrap();
             let mut seen = HashSet::new();
             let ignore: Vec<u32> = subs
@@ -429,12 +432,18 @@ pub fn main() -> Result<()> {
     Ok(())
 }
 
-fn print_metas(g: &mut Sodg) {
-    for (a, v) in g.kids(0).unwrap() {
-        if a.starts_with('+') {
-            info!("  {a}: {}", g.data(v).unwrap().to_utf8().unwrap())
+fn print_metas(g: &mut Sodg) -> Result<()> {
+    match g.kids(0) {
+        Ok(vec) => for (a, v) in vec {
+            if a.starts_with('+') {
+                info!("  {a}: {}", g.data(v)?.to_utf8()?)
+            }
+        }
+        Err(e)=> {
+            warn!("  {}", e)
         }
     }
+    Ok(())
 }
 
 fn inspect_v(g: &mut Sodg, v: u32, indent: usize, seen: &mut HashSet<u32>) {
